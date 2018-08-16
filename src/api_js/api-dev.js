2721a4f8d97a244c33cb293eafa257b42542f526
yjb <534111616@qq.com>
2018-08-16 19:56:08 +0800

    升级到最新版本
    Signed-off-by: yjb <534111616@qq.com>

--- src/api_js/api-dev.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hasee on 2017/7/26.
- */
-
-export default {
-  cd:'http://uat.zaichengdu.com',
-  wh:'http://uat.cdwh.org:7878',
-  cq:'http://uat.zaichongqing.com:7878'
-}
--- src/api_js/api-pro.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by hasee on 2017/7/26.
- */
-export default {
-  cd:'http://www.zaichengdu.com',
-  wh:'http://www.cdwh.org',
-  cq:'http://www.zaichongqing.com'
-}
