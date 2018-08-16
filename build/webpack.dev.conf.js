d34435bd7148bc3bada1de5197cc7177498d8e0b
yjb <534111616@qq.com>
2018-08-16 16:01:20 +0800

    add build

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,70 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var path = require('path');
+var glob = require('glob');
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+});
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+   /* new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),*/
+    new FriendlyErrorsPlugin()
+  ]
+})
+
+
+function getEntry(globPath) {
+  var entries = {}, basename, tmp, pathname;
+
+  glob.sync(globPath).forEach(function (entry) {
+    basename = path.basename(entry, path.extname(entry));
+    tmp = entry.split('/').splice(-3);
+    pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+    entries[pathname] = entry;
+  });
+
+  return entries;
+}
+var pages = getEntry('./src/html/**/*.html');
+
+for (var pathname in pages) {
+
+
+  // 配置生成的html文件，定义路径等
+  var conf = {
+    filename: pathname + '.html',
+    template: pages[pathname],   // 模板路径
+    inject: true              // js插入位置
+
+  };
+  
+  if (pathname in module.exports.entry) {
+    conf.chunks = ['manifest', 'vendor',pathname];
+    conf.hash = true;
+  }
+
+  module.exports.plugins.push(new HtmlWebpackPlugin(conf));
+}
